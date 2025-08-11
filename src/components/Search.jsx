8812c1876a14eda1b93be4f214fdd9b1a45391ae
Nwabueze Paul <paulnwabueze282@gmail.com>
2025-08-11 23:06:47 +0100

    syntax changes

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,7 +4,7 @@ const Search = ({ searchTerm, setSearchTerm }) => {
   return (
     <div className="search">
       <div>
-        <img src="search.svg" alt="search" />
+        <img src="Movie-Api2/search.svg" alt="search" />
 
         <input
           type="text"
